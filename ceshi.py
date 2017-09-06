162d311360b4f5ce7c5241e7e92a32581b9db9e4
Youngkl0726 <yklguge@gmail.com>
2017-09-06 22:22:44 +0800

    增加道路分割信息

--- ceshi.py
+++ ceshi.py
@@ -56,6 +56,20 @@ def get_ap_info(filename):
     ap_file.close()
     return ap_pos
 
+def get_3d_wcor(u, v, z, rwc, ow):
+    x = (v - cx) * z * invfx
+    y = (u - cy) * z * invfy
+    x3dc = [[] for k in range(3)]
+    x3dc[0].append(x)
+    x3dc[1].append(y)
+    x3dc[2].append(z)
+    mat_3dc = np.mat(x3dc)
+    mat_res = rwc.dot(mat_3dc) + ow
+    # print mat_res[0][0], mat_res[2][0]
+    list_res = mat_res.tolist()
+    return list_res
+
+
 # Visualize camera trajectory, MapPoints in pictures and AP boxes of cars
 def draw_points(camera_traj, x_p, y_p, ap_pos):
     sleep_t = 0.001
@@ -63,11 +77,16 @@ def draw_points(camera_traj, x_p, y_p, ap_pos):
              'deepskyblue', 'royalblue', 'violet', 'purple', 'green', 'chocolate']
     cars = []
     color_id = 0
+    road_left_all_3dx = []
+    road_left_all_3dy = []
+    road_right_all_3dx = []
+    road_right_all_3dy = []
     for i in range(271):
         fig1 = plt.figure(1)
         plt.pause(sleep_t)
         plt.clf()
         plt.axis([-100, 300, -100, 500])
+        plt.scatter(x=x_p[i], y=y_p[i], marker='o')
         plt.plot(x_p[0:i], y_p[0:i])
         existing_ids = []
         camera_t = camera_traj[i]
@@ -84,12 +103,56 @@ def draw_points(camera_traj, x_p, y_p, ap_pos):
             t[k].append(float(camera[3 + k * 4]))
         mat_t = np.mat(t)
         rwc = mat_r.transpose()
-        # print mat_r
-        # print mat_t
         ow = (-rwc).dot(mat_t)
-        # print rwc.dot(mat_t)
-        # print ow
-        print "i is :", i, len(ap_pos[i])
+
+        # Get depth map
+        img_dir = './kitti_disp_gen/04/'
+        img_name = '{:0>6d}.png'.format(i)
+        image_dir = path.join(img_dir, img_name)
+        img = np.array(Image.open(image_dir))
+        img = img / 256
+        img_depth = bf / img
+        # print img_depth
+        # print np.max(img_depth), np.min(img_depth)
+
+        road_left_curr_3dx = []
+        road_left_curr_3dy = []
+        road_right_curr_3dx = []
+        road_right_curr_3dy = []
+        seg_img_dir = './Demo04/ideal_result/'
+        seg_image_dir = path.join(seg_img_dir, img_name)
+        seg_img = np.array(Image.open(seg_image_dir))
+        h = seg_img.shape[0]
+        w = seg_img.shape[1]
+        uleft = 0
+        vleft = 0
+        uright = 0
+        vright = 0
+        for j in range(h - 1, -1, -1):
+            for k in range(w):
+                if seg_img[j][k] == 255:
+                    uleft = j
+                    vleft = k
+                    break
+            for k in range(w - 1, -1, -1):
+                if seg_img[j][k] == 255:
+                    uright = j
+                    vright = k
+                    break
+            zleft = -img_depth[uleft][vleft]
+            left_res = get_3d_wcor(uleft, vleft, zleft, rwc, ow)
+            # print left_res
+            zright = -img_depth[uright][vright]
+            right_res = get_3d_wcor(uright, vright, zright, rwc, ow)
+            road_left_curr_3dx.append(-left_res[0][0])
+            road_left_curr_3dy.append(-left_res[2][0])
+            road_right_curr_3dx.append(-right_res[0][0])
+            road_right_curr_3dy.append(-right_res[2][0])
+        plt.plot(road_left_curr_3dx[0:], road_left_curr_3dy[0:], c='black')
+        plt.plot(road_right_curr_3dx[0:], road_right_curr_3dy[0:], c='darkcyan')
+        # print "size is: ", len(road_left_curr_3dx)
+
+        # print "i is :", i, len(ap_pos[i])
         for line in (ap_pos[i]):
             line = line.split(' ')
             car_id = int(line[0])
@@ -99,30 +162,23 @@ def draw_points(camera_traj, x_p, y_p, ap_pos):
             right = int(line[3])
             bottom = int(line[4])
 
-            img_dir = '/Users/youngkl/Desktop/Demo/kitti_disp_gen/04/'
-            img_name = '{:0>6d}.png'.format(i)
-            image_dir = path.join(img_dir, img_name)
-            img = np.array(Image.open(image_dir))
-            img = img/256
-            img_depth = bf/img
-            # print img_depth
-            # print np.max(img_depth), np.min(img_depth)
             total_x = 0.0
             total_y = 0.0
             cnt = 0.0
             for u in range(top, bottom):
                 for v in range(left, right):
                     z = -img_depth[u][v]
-                    x = (v-cx)*z*invfx
-                    y = (u-cy)*z*invfy
-                    x3dc = [[] for k in range(3)]
-                    x3dc[0].append(x)
-                    x3dc[1].append(y)
-                    x3dc[2].append(z)
-                    mat_3dc = np.mat(x3dc)
-                    mat_res = rwc.dot(mat_3dc) + ow
-                    # print mat_res[0][0], mat_res[2][0]
-                    list_res = mat_res.tolist()
+                    list_res = get_3d_wcor(u,v,z,rwc,ow)
+                    # x = (v-cx)*z*invfx
+                    # y = (u-cy)*z*invfy
+                    # x3dc = [[] for k in range(3)]
+                    # x3dc[0].append(x)
+                    # x3dc[1].append(y)
+                    # x3dc[2].append(z)
+                    # mat_3dc = np.mat(x3dc)
+                    # mat_res = rwc.dot(mat_3dc) + ow
+                    # # print mat_res[0][0], mat_res[2][0]
+                    # list_res = mat_res.tolist()
                     # print list_res
                     total_x -= list_res[0][0]
                     total_y -= list_res[2][0]
@@ -136,6 +192,7 @@ def draw_points(camera_traj, x_p, y_p, ap_pos):
                     m.cor_x.append(total_x / cnt)
                     m.cor_y.append(total_y / cnt)
                     plt.plot(m.cor_x[0:], m.cor_y[0:], c=color[m.color])
+                    plt.scatter(x=m.cor_x[-1], y=m.cor_y[-1], c=color[m.color], marker='o')
                     break
             if flag == 0:
                 new_car = Cars(color_id, car_id, total_x / cnt, total_y / cnt)
@@ -153,11 +210,11 @@ def draw_points(camera_traj, x_p, y_p, ap_pos):
                 cars.remove(m)
 
         fig2 = plt.figure(2)
-        plt.axis([0, 1250, 350, 0])
+        # plt.axis([0, 1250, 350, 0])
         plt.pause(sleep_t)
         plt.clf()
 
-        file_dir = '/Users/youngkl/Desktop/Demo/Demo04/image_0/'
+        file_dir = './Demo04/image_0/'
         image_name = '{:0>6d}.png'.format(i)
         img = mpimg.imread(path.join(file_dir, image_name))
         plt.imshow(img, cmap='gray')
--- test.py
+++ test.py
@@ -1,6 +1,9 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from os import path
 from PIL import Image
 import numpy as np
-import matplotlib.pyplot as plt
+
 
 bf = 386.1448
 fx = 718.856
@@ -11,56 +14,150 @@ invfx = 1.0/fx
 invfy = 1.0/fy
 # print invfx, invfy
 
-img = np.array(Image.open('/Users/youngkl/Desktop/Demo/kitti_disp_gen/18/000000.png'))
-# print img
-img = img/256
+def get_3d_wcor(u, v, z, rwc, ow):
+    x = (v - cx) * z * invfx
+    y = (u - cy) * z * invfy
+    x3dc = [[] for k in range(3)]
+    x3dc[0].append(x)
+    x3dc[1].append(y)
+    x3dc[2].append(z)
+    mat_3dc = np.mat(x3dc)
+    mat_res = rwc.dot(mat_3dc) + ow
+    # print mat_res[0][0], mat_res[2][0]
+    list_res = mat_res.tolist()
+    return list_res
 
-depth = bf/img
-# print depth[100][100]
-# print depth
 
 def get_camera_traj(filename):
     camera_file = open(filename)
     x_p = []
     y_p = []
-    camera_traj = [[] for i in range(10)]
-    for i in range(10):
+    camera_traj = [[] for i in range(1801)]
+    for i in range(271):
         camera_line = camera_file.readline()
         camera_line = camera_line.strip()
         camera_line = camera_line.split(" ")
-        cam = []
-        for j in range(12):
-            cam.append(float(camera_line[j]))
-        cam.append(0.0)
-        cam.append(0.0)
-        cam.append(0.0)
-        cam.append(1.0)
-        r = [[] for k in range(4)]
-        for k in range(4):
-            for j in range(4):
-                r[k].append(float(cam[j+k*4]))
-        # print r
-        mat_r = np.mat(r)
-        # print mat_r
-        print mat_r.I
-        # print mat_r.dot(mat_r.I)
-        # print mat_r.transpose()
-        # print camera_line
-
         camera_traj[i].append(camera_line)
-        # print camera_traj
         x = float(camera_line[3])
         y = float(camera_line[11])
         x_p.append(x)
         y_p.append(y)
     camera_file.close()
-    # print camera_traj
-    return x_p, y_p
+    return camera_traj, x_p, y_p
 
-camera_f = r'./Demo00/CameraTrajectory.txt'
-get_camera_traj(camera_f)
-# for i in range(img.shape[0]):
-#     for j in range(img.shape[1]):
-#         print img[i][j],
-#     print ""
+camera_file = r'./Demo04/CameraTrajectory.txt'
+camera_traj, camera_x, camera_y = get_camera_traj(camera_file)
 
+sleep_t = 0.001
+for i in range(271):
+    fig1 = plt.figure(1)
+    plt.pause(sleep_t)
+    plt.clf()
+    plt.axis([-100, 300, -100, 500])
+    camera_t = camera_traj[i]
+    camera = camera_t[0]
+    r = [[] for k in range(3)]
+    for k in range(3):
+        for j in range(3):
+            r[k].append(float(camera[j + k * 4]))
+    mat_r = np.mat(r)
+    t = [[] for k in range(3)]
+    for k in range(3):
+        t[k].append(float(camera[3 + k * 4]))
+    mat_t = np.mat(t)
+    rwc = mat_r.transpose()
+    ow = (-rwc).dot(mat_t)
+    # Get depth map
+    img_dir = './kitti_disp_gen/04/'
+    img_name = '{:0>6d}.png'.format(i)
+    image_dir = path.join(img_dir, img_name)
+    img = np.array(Image.open(image_dir))
+    img = img / 256
+    img_depth = bf / img
+
+    road_left_curr_3dx = []
+    road_left_curr_3dy = []
+    road_right_curr_3dx = []
+    road_right_curr_3dy = []
+    seg_img_dir = './Demo04/ideal_result/'
+    seg_image_dir = path.join(seg_img_dir, img_name)
+    seg_img = np.array(Image.open(seg_image_dir))
+    h = seg_img.shape[0]
+    w = seg_img.shape[1]
+    uleft = 0
+    vleft = 0
+    uright = 0
+    vright = 0
+    # print seg_img
+    # for j in range(h):
+    #     flag = 0
+    #     for k in range(w):
+    #         if seg_img[j][k]==0:
+    #             flag=1
+    #     if flag == 0:
+    #         print i, j
+    for j in range(h - 1, -1, -1):
+        for k in range(w):
+            if seg_img[j][k] == 255:
+                # print "j is: ", j, "k is: ",k
+                uleft = j
+                vleft = k
+                break
+        for k in range(w - 1, -1, -1):
+            if seg_img[j][k] == 255:
+                # print "j is: ", j, "k is: ", k
+                uright = j
+                vright = k
+                break
+        # print uleft, vleft
+        zleft = -img_depth[uleft][vleft]
+        left_res = get_3d_wcor(uleft, vleft, zleft, rwc, ow)
+        # print left_res
+        zright = -img_depth[uright][vright]
+        right_res = get_3d_wcor(uright, vright, zright, rwc, ow)
+        road_left_curr_3dx.append(-left_res[0][0])
+        road_left_curr_3dy.append(-left_res[2][0])
+        road_right_curr_3dx.append(-right_res[0][0])
+        road_right_curr_3dy.append(-right_res[2][0])
+    plt.plot(road_left_curr_3dx[0:], road_left_curr_3dy[0:], c='black')
+    plt.plot(road_right_curr_3dx[0:], road_right_curr_3dy[0:], c='darkcyan')
+    print "size is: ", len(road_left_curr_3dx)
+    if len(road_left_curr_3dx) != 370:
+        print i
+        break
+    print road_left_curr_3dx
+    print road_left_curr_3dy
+
+# img = np.array(Image.open('./Demo04/ideal_result/000000.png'))
+#
+# h = img.shape[0]
+# w = img.shape[1]
+# print h,w
+#
+# road_left_all_2dx = []
+# road_left_all_2dy = []
+# road_right_all_2dx = []
+# road_right_all_2dy = []
+# road_left_curr_2dx = []
+# road_left_curr_2dy = []
+# road_right_curr_2dx = []
+# road_right_curr_2dy = []
+# for j in range(h-1, -1, -1):
+#     for k in range(w):
+#         if img[j][k]==0:
+#             road_left_all_2dx.append(j)
+#             road_left_all_2dy.append(k)
+#             road_left_curr_2dx.append(j)
+#             road_left_curr_2dy.append(k)
+#             break
+#     for k in range(w-1, -1, -1):
+#         if img[j][k]==0:
+#             road_right_all_2dx.append(j)
+#             road_right_all_2dy.append(k)
+#             road_right_curr_2dx.append(j)
+#             road_right_curr_2dy.append(k)
+#             break
+#
+# # print road_left_all_2dx, road_left_all_2dy
+# # print road_right_all_2dx, road_right_all_2dy
+# print len(road_left_curr_2dx), len(road_right_curr_2dx)
