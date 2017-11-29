c04d169ee5a6d3a81a11bf92c53c214c0b00c271
Youngkl0726 <yklguge@gmail.com>
2017-11-29 21:24:50 +0800

    update

---
+++ monodepth/generate_train_list.py
@@ -0,0 +1,31 @@
+
+# txt_file = open('sz_train_list.txt', 'wb')
+# for i in xrange(4324):
+#     left_file = 'left/' + '{:0>6d}.png'.format(i)
+#     right_file = 'right/' + '{:0>6d}.png'.format(i)
+#     print left_file
+#     txt_file.write(left_file + ' ' + right_file + '\n')
+# txt_file.close()
+
+
+# txt_file = open('finetune_list.txt', 'wb')
+# for i in xrange(25058):
+#     left_file = 'stillcar_0/image_0/' + '{:0>6d}.png'.format(i*2)
+#     right_file = 'stillcar_0/image_1/' + '{:0>6d}.png'.format(i*2)
+#     print left_file
+#     txt_file.write(left_file + ' ' + right_file + '\n')
+# for i in xrange(2896):
+#     left_file = 'stillcar/image_0/' + '{:0>6d}.png'.format(i*2)
+#     right_file = 'stillcar/image_1/' + '{:0>6d}.png'.format(i*2)
+#     print left_file
+#     txt_file.write(left_file + ' ' + right_file + '\n')
+# txt_file.close()
+
+# txt_file = open('final_30000.txt', 'wb')
+# file = open('final.txt')
+# for i in xrange(30000):
+#     line = file.readline()
+#     line = line.strip()
+#     print line
+#     txt_file.write(line+'\n')
+# txt_file.close()
---
+++ monodepth_test.py
@@ -0,0 +1,100 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from os import path
+import numpy as np
+from PIL import Image
+import cv2
+# def convert_disps_to_depths_kitti(pred_disparities):
+#     gt_depths = []
+#     pred_depths = []
+#     pred_disparities_resized = []
+#
+#     for i in range(len(pred_disparities)):
+#         pred_disp = pred_disparities[i]
+#         height, width = pred_disp.shape
+#         pred_disp = width * cv2.resize(pred_disp, (width, height), interpolation=cv2.INTER_LINEAR)
+#
+#         pred_disparities_resized.append(pred_disp)
+#         pred_depth = 512 * 0.54 / pred_disp
+#         pred_depths.append(pred_depth)
+#     return pred_depths
+#
+#
+# disp_image_dir = ('./monodepth/001143_disp.png')
+# disp_img = np.array(Image.open(disp_image_dir))
+# pred_depths = convert_disps_to_depths_kitti(disp_img)
+
+
+# print pred_depths.shape
+# print pred_depths[401][283]
+
+disp_image_dir = ('./monodepth/disparities.npy')
+disp = np.load(disp_image_dir)
+print disp.shape
+
+
+def get_ap_log(filename):
+    file = open(filename)
+    res_line = [[] for i in xrange(5906)]
+    for i in xrange(5906):
+        # print i
+        line = file.readline()
+        line = line.strip()
+        line = line.split(" ")
+        # print len(line)
+        # print line
+        res_line[i].append(int(line[0]))
+        res_line[i].append(int(line[1]))
+        res_line[i].append(float(line[18]))
+        res_line[i].append(float(line[19]))
+        res_line[i].append(float(line[20]))
+        res_line[i].append(float(line[21]))
+    # print res_line
+    return res_line
+
+bf = 26.0
+
+def draw(ap_res):
+    depth = []
+    depth_id = []
+    depth_num = 0
+    sleep_t = 0.001
+    for i in range(3435):
+        # print ap_res[i]
+        # Get depth map
+        person_id = ap_res[i][1]
+        if person_id != 48:
+            continue
+        fig1 = plt.figure(1)
+        plt.pause(sleep_t)
+        plt.clf()
+        pic_id = ap_res[i][0]
+        disp_i = cv2.resize(disp[pic_id], (878, 493), interpolation=cv2.INTER_LINEAR)
+        depth_img = bf / (disp_i * 512.0)
+        point1_x = ap_res[i][2]
+        point1_y = ap_res[i][3]
+        point2_x = ap_res[i][4]
+        point2_y = ap_res[i][5]
+        mid_x = int((point1_x + point2_x) / 2)
+        mid_y = int((point1_y + point2_y) / 2)
+        mid_x = min(493, mid_x)
+        mid_y = min(878, mid_y)
+
+        z = depth_img[mid_y][mid_x]
+        # if z > 50:
+        #     continue
+        depth.append(z)
+        depth_id.append(depth_num)
+        depth_num += 1
+        plt.plot(depth_id[0:], depth[0:], c='blue')
+    # plt.pause(10)
+    plt.show()
+
+def main():
+    ap_file = r'./sz2/stillcar.log'
+    ap_res = get_ap_log(ap_file)
+    draw(ap_res)
+
+
+if __name__ == '__main__':
+    main()
